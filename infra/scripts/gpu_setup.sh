e8585efb4a38a80b36d5fbbe242a5dfb4577ed36
tohio <tohio@hotmail.com>
2026-03-26 22:12:17 -0400

    fix gpcheck issue

--- infra/scripts/gpu_setup.sh
+++ infra/scripts/gpu_setup.sh
@@ -58,6 +58,7 @@ else
         | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
     curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+        | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
         | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
     sudo apt-get update -qq
